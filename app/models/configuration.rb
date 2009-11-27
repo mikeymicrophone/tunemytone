40465d5b7a215a480cedb49bfc0a7688042b0773
Mike Schwab <mike.schwab@gmail.com>
2009-11-27 14:23:49 -0500

    artist participation in things

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -3,4 +3,8 @@ class Configuration < ActiveRecord::Base
   has_many :elements
   has_many :uses, :as => :specification, :class_name => 'Element'
   has_many :sounds
+  
+  def instruments
+    elements.instrumental.map(&:specification)
+  end
 end
--- app/models/element.rb
+++ app/models/element.rb
@@ -2,4 +2,6 @@ class Element < ActiveRecord::Base
   belongs_to :configuration
   belongs_to :user
   belongs_to :specification, :polymorphic => true
+  
+  named_scope :instrumental, :conditions => {:specification_type => 'Instrument'}
 end
