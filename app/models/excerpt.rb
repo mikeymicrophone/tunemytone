6fe1f492b583f21053483006127f0ec66ace6459
Mike Schwab <mike.schwab@gmail.com>
2009-12-09 22:03:29 -0500

    upload recordings

--- app/models/excerpt.rb
+++ app/models/excerpt.rb
@@ -2,4 +2,8 @@ class Excerpt < ActiveRecord::Base
   belongs_to :user
   belongs_to :context, :polymorphic => true
   has_many :sounds, :as => :example
+  
+  def name
+    description + ' in ' + context.name
+  end
 end
